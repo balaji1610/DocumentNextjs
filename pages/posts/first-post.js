ec40bfeeaf39c566620c62028f744ebdcadeb1a0
balaji <balaji@alphabsolutions.com>
2022-10-14 15:43:25 +0530

    profile

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,13 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
-import Layout from "../../../components/layout";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
       <Layout>
-        <Head>
+    
+        {/* <Head>
           <title>First Post</title>
         </Head>
         <Script
@@ -16,14 +17,15 @@ export default function FirstPost() {
           strategy="lazyOnload"
           onLoad={() => console.log("Script Executed")}
         />
-        <h1>First Post</h1>
-        <Link href="/">Back To Home</Link>
-        <Image
+        <h1>First Post</h1> */}
+        {/* <Link href="/">Back To Home</Link> */}
+        {/* <Image
           src="/images/avatar.jpg"
           alt="profile"
           height={144}
           width={144}
-        />
+        /> */}
+    
       </Layout>
     </>
   );
