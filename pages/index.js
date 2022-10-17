d206a6abd8d293a961f5348d6090e5923722aa5f
balaji1610 <balajiece1610>
2022-10-17 06:45:07 +0530

    Link

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import { getSortedPostsData } from "../lib/posts";
+import Date from "../components/date";
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -47,6 +48,22 @@ export default function Home({ allPostsData }) {
             ))}
           </ul>
         </div>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
